c1574edcbe9692f95bb3126592418aad1876b786
saurabhje <work.saurabh@outlook.com>
2023-06-14 18:08:25 +0530

    Shuffling the cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React , {useState} from "react";
+import CardGrids from "./component/cardgrids";
 
 const App = () =>{
+    const [CurrentScore, setCurrentScore] = useState(0);
+    const [BestScore , setBestScore] = useState(0);
+    const [clickedBooks, setclickedBooks] = useState([]);
 
+    const handleClick = (e) =>{
+        const bookName = e.target.parentNode.lastChild.textContent;
+        playRound(bookName);
+    }
+
+    const playRound = (bookname) =>{
+        if(clickedBooks.includes(bookname)){
+            resetGame();
+        } else{
+            const newscore =CurrentScore + 1;
+            setCurrentScore(newscore);
+            if(newscore > BestScore) setBestScore(newscore);
+            setclickedBooks((prevState)=> [...prevState, bookname]);
+        }
+    }
+    
+    const resetGame = () =>{
+        setclickedBooks([]);
+        setCurrentScore(0);
+    }
+
+    return (
+        <>
+            <CardGrids handleClick = {handleClick} />
+        </>
+    )
 }
 
 export default App;
--- src/component/cardgrids.js
+++ src/component/cardgrids.js
@@ -1,57 +1,84 @@
-import React from "react";
+import React , {useState , useEffect}from "react";
+import style from "../styles/cardgrid.css";
 
-const CardGrids = () =>{
-    const Cards = [
+const CardGrids = ({handleClick}) =>{
+    const [Cards, setCards] = useState([]);
+    
+    useEffect(()=>{
+        setCards(bookcards);
+    },[])
+
+    const bookcards = [
         <div className="cardbox">
-            <img src = {require('../book1.jpg')} alt=""></img>
+            <img src = {require('../images/book1.jpg')} alt=""></img>
             <p>Crime & Punishment</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book2.jpg')} alt=""></img>
+            <img src = {require('../images/book2.jpg')} alt=""></img>
             <p>1984</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book3.jpg')} alt=""></img>
+            <img src = {require('../images/book3.jpg')} alt=""></img>
             <p>The Noise of Time</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book4.jpg')} alt=""></img>
+            <img src = {require('../images/book4.jpg')} alt=""></img>
             <p>The Brothers Karamazov</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book5.jpg')} alt=""></img>
+            <img src = {require('../images/book5.jpg')} alt=""></img>
             <p>To Kill A Monkingbird</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book6.jpg')} alt=""></img>
+            <img src = {require('../images/book6.jpg')} alt=""></img>
             <p>Fahrenheit 451</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book7.jpg')} alt=""></img>
+            <img src = {require('../images/book7.jpg')} alt=""></img>
             <p>Lord of The Flies</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book8.jpg')} alt=""></img>
+            <img src = {require('../images/book8.jpg')} alt=""></img>
             <p>War and Peace</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book9.jpg')} alt=""></img>
+            <img src = {require('../images/book9.jpg')} alt=""></img>
             <p>The Old Man and The Sea</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book10.jpg')} alt=""></img>
+            <img src = {require('../images/book10.jpg')} alt=""></img>
             <p>Metamorphosis</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book11.jpg')} alt=""></img>
+            <img src = {require('../images/book11.jpg')} alt=""></img>
             <p>Man's Search For Meaning</p>
         </div>,
         <div className="cardbox">
-            <img src = {require('../book12.jpg')} alt=""></img>
+            <img src = {require('../images/book12.jpg')} alt=""></img>
             <p>Great Expectation</p>
         </div>,
+    ];
 
-    ]
+    const shuffleArray = (array) => {
+        return [...array].sort(() => Math.random() - 0.5)
+    }
+
+    const handleCardClick = (e) =>{
+        setCards(shuffleArray(bookcards));
+        handleClick(e);
+    }
+
+    return (
+        <>
+        <div className="card-container">
+            {Cards.map((card) => (
+             <div onClick={handleCardClick}>
+                 {card}
+             </div>
+            ))}
+        </div>
+        </>
+    )
 }
 
 export default CardGrids;
